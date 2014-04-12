9e7680b15dff0084bac79a56bd7e733103d15df0
Jeremy Perret <jeremy@devster.org>
2014-04-12 18:41:57 +0200

    change the way ansible is installed

--- debian/install_ansible.sh
+++ debian/install_ansible.sh
@@ -2,9 +2,9 @@
 
 if ! which ansible > /dev/null; then
     echo "Install Ansible"
-    sudo apt-add-repository -y ppa:rquillo/ansible
-    sudo apt-get update
-    sudo apt-get install -y ansible
+    sudo apt-get install python-setuptools python-dev
+    sudo easy_install pip
+    sudo pip install ansible
 fi
 
 # return 0 if ansible is well installed
